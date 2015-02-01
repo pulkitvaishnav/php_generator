2608c364824a0b4d5e8ae5a0fba529b31abd9401
pulkitvaishnav <pulkitvaishnav@gmail.com>
2015-02-01 19:52:29 +0530

    PHP-snipp with database

--- app/controllers/generators_controller.rb
+++ app/controllers/generators_controller.rb
@@ -1,99 +1,44 @@
 class GeneratorsController < ApplicationController
-	#include GeneratorsHelper
+	include ApplicationHelper
 	def index
 		
 	end
 	def create
-		@input_code = params[:input_code]
-		#input = input_code(@user_input)
-		#generate_php_code(input)
-		redirect_to action: "show"
 	end
 	def show
-		@input_code = params[:input_code]
-		#send_file params[:input_code]
-		respond_to do |format|
-			format.html
-			format.json
-		end
 	end
 	def new
 		
 	end
 	def form
 		@name = params[:name]
-		#user_input = @name
-		#$/ = "</form>"
-		#user_input = user_input.try(:split,"\n")
+		@database_name = params[:database_name]
+		@table_name = params[:table_name]
+		@database_attr = params[:database_attr]
 		input_array = Array.new
 		@output_array = Array.new
 		@name.to_s.try(:split, "\n").each do |i|
 			each_element = i.try(:split, /[<>\s]/)
 			input_array.push each_element
-			#logger.debug
 		end
-		#logger.debug "<?php \ninclude(database_connect.php);"
-		@output_array.push "<?php \ninclude(database_connect.php);"	
 		input_array.each do |each_element|
+			@output_array.push "<?php"
+			@output_array.push "\tinclude(database_connect.php);"	
 			if each_element.select{|type| type.match(/button=.*/)}
-				#if each_element.include?('type="submit"')
-				#logger.debug "\tif(isset($_POST['submit'])){"
 				@output_array.push "\tif(isset($_POST['submit'])){"
 				break;
-				#end
 			end
 		end
 		counter = 0
-		value_of_attribute = Array.new
-		input_array.each do |each_element|
-			name = each_element.select{|type| type.match(/name=.*/)}
-			
-			#$input = $_POST['input'];
-			if name.length >= 1
-				name = name.to_s
-				name = name.slice(9 .. -5)
-				#value_of_attribute.push name
-				#logger.debug "\t\t$#{name} = $_POST['#{name}'];"
-				if !(name .eql? "submit")
-					value_of_attribute.push name
-					@output_array.push "\t\t$#{name} = $_POST['#{name}'];"	
-					counter = counter + 1
-				else
-				 	break
-			 	end
-			end
+		value_of_attribute = Array.new		
+		@output_array, counter, value_of_attribute, radiocounter = get_attributes(counter, value_of_attribute, input_array)
+		sql = input_query(counter, value_of_attribute, radiocounter, @database_name, @table_name, @database_attr)
+		
+		if @name.to_s.length > 0
+			@output_array.push sql.to_s
+			@output_array.push "\t\tmysqli_query($dbconnect, $sql);"
+			@output_array.push "\t}"
+			@output_array.push "?>"
 		end
-		intial_query = "\t\tINSERT INTO `<database_name>`.`<table_name>`"
-		no_of_attribute = "("
-		for i in 0..counter-1
-			if i < counter-1
-				no_of_attribute= no_of_attribute + "`attribute_#{i+1}`, "	
-			else
-				no_of_attribute= no_of_attribute + "`attribute_#{i+1}`"
-			end
-			
-		end
-		no_of_attribute = no_of_attribute + ") "
-		values_for_attributes = "VALUES("
-		len= value_of_attribute.length
-		for i in 0..len - 1
-			if i < len - 1
-				values_for_attributes = values_for_attributes + "'$#{value_of_attribute[i]}', "
-			else
-				values_for_attributes = values_for_attributes + "'$#{value_of_attribute[i]}'"
-			end
-		end
-		values_for_attributes = values_for_attributes + ");"
-		sql = intial_query + no_of_attribute + values_for_attributes
-		#logger.debug sql
-		@output_array.push sql.to_s
-		#logger.debug "\t\tmysqli_query($dbconnect, $sql);"
-		@output_array.push "\t\tmysqli_query($dbconnect, $sql);"
-		#logger.debug "\t}"
-		@output_array.push "\t}"
-		#logger.debug "?>"
-		@output_array.push "?>"
-		#logger.debug @output_array
-		#render text: @output_array.to_s
 	end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,72 @@
 module ApplicationHelper
+	def input_query(counter, value_of_attribute, radiocounter, database_name, table_name, database_attr)
+			if database_name.to_s.length > 0
+				intial_query = "INSERT INTO `#{database_name}`.`#{table_name}`"
+			else
+				intial_query = "INSERT INTO `<database_name>`.`<table_name>`"
+			end
+			no_of_attribute = "("
+			no_of_attribute = attributes_name(database_name, table_name, database_attr, counter, radiocounter, no_of_attribute)
+			no_of_attribute = no_of_attribute + ") "
+			values_for_attributes = "VALUES("
+			len = value_of_attribute.length
+			for i in 0..len - 1
+				if i < len - 1
+					values_for_attributes = values_for_attributes + "'$#{value_of_attribute[i]}', "
+				else
+					values_for_attributes = values_for_attributes + "'$#{value_of_attribute[i]}'"
+				end
+			end
+			#no_of_attribute = attributes_name(database_name, table_name, database_attr, counter, no_of_attribute)
+			values_for_attributes = values_for_attributes + ");"
+			return sql = "\t\t$sql = " + intial_query + no_of_attribute + values_for_attributes
+			
+	end
+	def get_attributes(counter, value_of_attribute, input_array)
+		radiocounter = 0
+		input_array.each do |each_element|
+			name = each_element.select{|type| type.match(/name=.*/)}
+			if name.length >= 1
+				name = name.to_s.slice(9 .. -5)
+				if each_element.select{|type| type.match(/name=radio.*/)}
+					radiocounter = radiocounter + 1
+				end
+				if !(name .eql? "submit")
+					value_of_attribute.push name
+					@output_array.push "\t\t$#{name} = $_POST['#{name}'];"	
+					counter = counter + 1
+				else
+				 	break
+			 	end
+			end
+		end
+		return @output_array, counter, value_of_attribute, radiocounter
+	end
+	def attributes_name(database_name, table_name, database_attr, counter, radiocounter, no_of_attribute)
+	 	if database_name.to_s.length > 0
+	 		database_attr.to_s.try(:split, ",").each_with_index do |element, index|
+
+	 			if index < counter-1 && radiocounter<2
+	 				no_of_attribute = no_of_attribute + "`#{element}`, "
+	 			else
+	 				if radiocounter>2
+	 					next	
+	 				end
+	 				no_of_attribute = no_of_attribute + "`#{element}`"
+	 			end
+
+	 		end
+	 	else
+	 		for i in 0..counter-1
+	 			if i < counter-1
+	 				no_of_attribute= no_of_attribute + "`attribute_#{i+1}`, "	
+	 			else
+	 				no_of_attribute= no_of_attribute + "`attribute_#{i+1}`"
+	 			end
+				
+	 		end
+	 	end
+	 	return no_of_attribute
+	end
+		
 end
